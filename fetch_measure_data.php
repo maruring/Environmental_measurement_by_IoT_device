c670bb8a5e853e5f9c91ea104a7b971a7f212c8c
maruring <maruring3745@gmail.com>
2022-05-07 14:27:40 +0900

    データ参照ページ追加

--- fetch_measure_data.php
+++ fetch_measure_data.php
@@ -22,10 +22,8 @@ $sql = "SELECT device_id FROM device_info WHERE device_name=:device_name";
 $sth = $dbh -> prepare($sql);
 $sth -> bindValue(':device_name', $device_name);
 $sth -> execute();
-//$sth = $dbh -> query($sql);
 $device_id = $sth->fetch(PDO::FETCH_ASSOC);
 $device_id = $device_id['device_id'];
-print_r($device_id);
 
 // device_idから測定データを取得
 $sql = null;
@@ -34,8 +32,7 @@ $sth = null;
 $sql = "SELECT datetime, light, volume, temp, humi FROM measure_data WHERE device_id=:device_id";
 $sth = $dbh -> prepare($sql);
 $sth -> bindValue(':device_id', $device_id);
-$sth->execute();
-$sth = $dbh -> query($sql);
+$sth -> execute();
 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
 print_r($result);
