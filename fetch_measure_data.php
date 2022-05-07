b7e9c831c28e5a2c0d0c81f8a5a1ac564efa97cd
maruring <maruring3745@gmail.com>
2022-05-07 15:35:09 +0900

    データ参照ページ追加

--- fetch_measure_data.php
+++ fetch_measure_data.php
@@ -7,76 +7,6 @@
     <body>
         <h2><?php echo htmlspecialchars($_GET['device_name']); ?>のデータ</h2>
 
-        <?php
-        //接続するデータベースの設定
-        $dsn = 'mysql:dbname=iotdata;host=192.168.2.117';
-        $user = 'maru3745';
-        $password = 'maruring';
-
-        $device_name = $_GET['device_name'];
-
-        //データベースと接続
-        try {
-            $dbh = new PDO($dsn, $user, $password);
-            //echo "DBに接続成功\n";
-        } catch (PDOException $e) {
-            echo "接続失敗: " . $e->getMessage() . "\n";
-            exit();
-        }
-
-
-        // getで受け取ったdevice_nameからdevice_idを取得
-        $sql = "SELECT device_id FROM device_info WHERE device_name=:device_name";
-        $sth = $dbh -> prepare($sql);
-        $sth -> bindValue(':device_name', $device_name);
-        $sth -> execute();
-        $device_id = $sth->fetch(PDO::FETCH_ASSOC);
-        $device_id = $device_id['device_id'];
-
-        // device_idから測定データを取得
-        $sql = null;
-        $sth = null;
-
-        $sql = "SELECT datetime, light, volume, temp, humi FROM measure_data WHERE device_id=:device_id";
-        $sth = $dbh -> prepare($sql);
-        $sth -> bindValue(':device_id', $device_id);
-        $sth -> execute();
-        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-        $datetimes = array_column($result, 'datetime');
-        $lights = array_column($result, 'light');
-        $temps = array_column($result, 'temp');
-        $humis = array_column($result, 'humi');
-
-        print_r($datetimes);
-        print_r($lights);
-        print_r($temps);
-        print_r($humis);
-
-        $kari = array(1,2);
-
-        //データを表で表示
-        $data_table = "<table>\n";
-            $data_table .= "<thead>\n";
-                $data_table .= "<tr>\n";
-                    $data_table .= "<th count( $kari )>データ一覧</th>\n";
-                $data_table .= "</tr>\n";
-            $data_table .= "</thead>\n";
-            $data_table .= "<tbody>\n";
-                $data_table .= "<tr>\n";
-                    $data_table .= "<td>aa</td>\n";
-                    $data_table .= "<td>bb</td>\n";
-                $data_table .= "</tr>\n";
-            $data_table .= "</tbody>\n";
-        $data_table = "</table>\n";
-
-        echo "{$data_table}";
-
-        //切断を閉じる
-        $sth = null;
-        $dbh = null;
-        ?>
-
         <table width="80%" border="1">
             <tr>
                 <th scope="col">時間</th>
@@ -84,11 +14,72 @@
                 <th scope="col">気温</th>
                 <th scope="col">湿度</th>
             </tr>
+            <?php
+                //接続するデータベースの設定
+                $dsn = 'mysql:dbname=iotdata;host=192.168.2.117';
+                $user = 'maru3745';
+                $password = 'maruring';
+
+                $device_name = $_GET['device_name'];
+
+                //データベースと接続
+                try {
+                    $dbh = new PDO($dsn, $user, $password);
+                    //echo "DBに接続成功\n";
+                } catch (PDOException $e) {
+                    echo "接続失敗: " . $e->getMessage() . "\n";
+                    exit();
+                }
+
+                // getで受け取ったdevice_nameからdevice_idを取得
+                $sql = "SELECT device_id FROM device_info WHERE device_name=:device_name";
+                $sth = $dbh -> prepare($sql);
+                $sth -> bindValue(':device_name', $device_name);
+                $sth -> execute();
+                $device_id = $sth->fetch(PDO::FETCH_ASSOC);
+                $device_id = $device_id['device_id'];
+
+                // device_idから測定データを取得
+                $sql = null;
+                $sth = null;
+
+                $sql = "SELECT datetime, light, volume, temp, humi FROM measure_data WHERE device_id=:device_id";
+                $sth = $dbh -> prepare($sql);
+                $sth -> bindValue(':device_id', $device_id);
+                $sth -> execute();
+                $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+                $datetimes = array_column($result, 'datetime');
+                $lights = array_column($result, 'light');
+                $temps = array_column($result, 'temp');
+                $humis = array_column($result, 'humi');
+
+                $kari = array(1,2);
+
+                //データを表で表示
+                $data_table = "<table>\n";
+                    $data_table .= "<thead>\n";
+                        $data_table .= "<tr>\n";
+                            $data_table .= "<th count( $kari )>データ一覧</th>\n";
+                        $data_table .= "</tr>\n";
+                    $data_table .= "</thead>\n";
+                    $data_table .= "<tbody>\n";
+                        $data_table .= "<tr>\n";
+                            $data_table .= "<td>aa</td>\n";
+                            $data_table .= "<td>bb</td>\n";
+                        $data_table .= "</tr>\n";
+                    $data_table .= "</tbody>\n";
+                $data_table = "</table>\n";
+
+                //切断を閉じる
+                $sth = null;
+                $dbh = null;
+                ?>
             <tr>
-                <td><?php print(htmlspecialchars($result['datetime'])); ?> </td>
-                <td><?php print(htmlspecialchars($result['light'])); ?> </td>
-                <td><?php print(htmlspecialchars($result['temp'])); ?> </td>
-                <td><?php print(htmlspecialchars($result['humi'])); ?> </td>
+                <td><?php print(htmlspecialchars($result['datetime'])); ?></td>
+                <td><?php print(htmlspecialchars($result['light'])); ?></td>
+                <td><?php print(htmlspecialchars($result['temp'])); ?></td>
+                <td><?php print(htmlspecialchars($result['humi'])); ?></td>
             </tr>
             
             <a href='index.html'>初期ページ</a>
