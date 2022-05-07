197e692cdbdbc3126d14b9ed9a2eb3951309d07a
maruring <maruring3745@gmail.com>
2022-05-07 16:07:08 +0900

    データ参照ページ追加

--- fetch_measure_data.php
+++ fetch_measure_data.php
@@ -54,14 +54,16 @@
                 $temps = array_column($result, 'temp');
                 $humis = array_column($result, 'humi');
 
-                $data_table = "<tr>\n";
-                    for( $i = 0; $i < count( $datetimes ); $i++ ){
-                        $data_table .="<td>$datetimes[$i]</td>\n";
-                        $data_table .="<td>$lights[$i]</td>\n";
-                        $data_table .="<td>$temps[$i]</td>\n";
-                        $data_table .="<td>$humis[$i]</td>\n";
-                    }
-                $data_table .= "</tr>\n";
+                for( $i = 0; $i < count( $datetimes ); $i++ ){
+                    $data_table = "<tr>\n";
+                        for( $i = 0; $i < 4; $i++ ){
+                            $data_table .="<td>$datetimes[$i]</td>\n";
+                            $data_table .="<td>$lights[$i]</td>\n";
+                            $data_table .="<td>$temps[$i]</td>\n";
+                            $data_table .="<td>$humis[$i]</td>\n";
+                        }
+                    $data_table .= "</tr>\n";
+                }
 
                 echo "{$data_table}";
 
