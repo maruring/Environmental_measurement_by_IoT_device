c11696b630a6c995f773bd9b89a6cd39878e10a5
maruring <maruring3745@gmail.com>
2022-05-07 13:38:49 +0900

    データ参照ページ追加

--- reference_page.php
+++ reference_page.php
@@ -28,13 +28,11 @@
             $device_names = $sth -> fetchAll(PDO::FETCH_ASSOC);
             $device_names = array_column($device_names, 'device_name');
 
-            print_r($device_names);
-
             //inputタグの作成
             $sampleDatalist = '<input type=\'text\' name=\'device_name\' autocomplete=\'on\' list=\'device_names\'>\n'; 
             $sampleDatalist .= '<datalist id=\'device_names\'>\n';
             for ( $i = 0; $i < count( $device_names ); $i++ ) {
-                $device_name_list .= '\t<option value=\'{$array[$i]}\'>{$array[$i]}</option>\n';
+                $device_name_list .= '\t<option value=\'{$device_names[$i]}\'>{$device_names[$i]}</option>\n';
             }
             $device_name_list .= '</datalist>\n';
             echo '{$device_name_list}';
